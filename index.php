c064e031479c1a8a297846b43e5d52d7cd284308
oldmanjacob <jacob@billsfireplace.com>
2024-10-29 15:54:10 -0700

    initialize

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-fisrt file
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <title>Davy Jones' Locker</title>
+    <meta name="description" content="Davy Jones' Locker is North Idaho's premier hookah lounge and activity center.">
+    <meta name="keywords" content="relevant, keywords, here">
+    <meta name="author" content="Your Name or Company Name">
+   
+    <meta property="og:title" content="Davy Jones' Locker">
+    <meta property="og:description" content="Davy Jones' Locker is North Idaho's premier hookah lounge and activity center.">
+    <meta property="og:image" content="URL to your preview image">
+    <meta property="og:url" content="URL to your website">
+    <meta property="og:type" content="website">
+   
+    <link rel="icon" href="path/to/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="path/to/apple-touch-icon.png">
+    
+    <link rel="stylesheet" href="css\normalize.css">
+    <!-- Font or Icon Libraries (optional) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!-- JavaScript Links (optional, or place before closing </body>) -->
+    <script defer src="path/to/script.js"></script>
+</head>
+<body>
+    <!-- Main Content Starts Here -->
+</body>
+</html>
